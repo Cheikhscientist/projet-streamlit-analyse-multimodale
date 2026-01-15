f0c17c7070b06d05774ac8cb17b11f02007dab43
Cheikh Sarr <cheikh.sarr@etu.univ-poitiers.fr>
2026-01-15 13:43:40 +0000

    Application complète et fonctionnelle

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from modules.csv_analysis import csv_page
-from modules.text_analysis import text_page
-from modules.image_analysis import image_page
+from csv_analysis import csv_page
+from text_analysis import text_page
+from image_analysis import image_page
 
 st.set_page_config(page_title="Analyse Multimodale", layout="wide")
 st.title("Application d’analyse multimodale")
